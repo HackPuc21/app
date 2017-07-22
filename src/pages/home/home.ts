52ca8d32acffdcfc9812d04776f2d63620def728
Ramon Portela <portela.ramon96@gmail.com>
2017-07-22 11:14:30 -0700

    alteração layout/css
    alteração css

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CarrinhoCompras } from '../list/list';
+import { QrCodePage } from '../qrcode/qrcode';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +11,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+    abrirTelaLista(){
+    this.navCtrl.push(CarrinhoCompras);
   }
 
 }
