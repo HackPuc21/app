5ac0362dd500feebef868c6dc2d5877675460ce8
Renan Oliveira <renan.oliveira_10@hotmail.com>
2017-07-22 03:20:04 -0300

    interagindo com a lista

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -5,33 +5,28 @@ import { NavController, NavParams } from 'ionic-angular';
   selector: 'page-list',
   templateUrl: 'list.html'
 })
-export class ListPage {
+export class CarrinhoCompras {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  itemsSelecionados : Array<{nome: string, codigoBarras:string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+    this.itemsSelecionados = [];
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
+  deletar(event: any, index : any) {
+    console.log(index);
+    this.itemsSelecionados.splice(index, 1)
+  }
+
+  abrirTelaCamera(){
+    this.navCtrl.push(CarrinhoCompras)
+  }
+
+  mock(event: any){
+    this.itemsSelecionados.push({
+        nome: 'item' + this.itemsSelecionados.length,
+        codigoBarras: this.itemsSelecionados.length.toString()
+      });
   }
 }
