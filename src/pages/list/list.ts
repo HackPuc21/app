62e3e4ce312cac792cb761dea47915f40d8228cf
Renan Oliveira <renan.oliveira_10@hotmail.com>
2017-07-22 23:34:06 -0300

    Filtro string de voz
    Filtra a string do reconhecimento de voz

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,43 +1,53 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController  } from 'ionic-angular';
 import { CameraPage } from '../camera/camera';
 import { QrCodePage } from '../qrcode/qrcode'
 import { VozPage } from '../voz/voz';
+import { SpeechRecognition} from '@ionic-native/speech-recognition';
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class CarrinhoCompras {
+  alertCtrl: AlertController;
   selectedItem: any;
   icons: string[]; 
   itemsSelecionados : Array<{nome: string, codigoBarras:string, quantidade:number}>;
   resolve: any;
+  speech: SpeechRecognition;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertController:AlertController, private speechModule: SpeechRecognition) {
     this.itemsSelecionados = [];
+    this.alertCtrl = alertController;
     this.resolve = (_text)=> {
         if(!_text) return;
         new Promise((ok, notOk) => {
-          this.itemsSelecionados  = this.itemsSelecionados ? this.itemsSelecionados : [];
-          console.log(this.itemsSelecionados);
-          
-          let index = this.itemsSelecionados.findIndex((item) => {
-            return item.codigoBarras == _text
-          })
-
-          if(index >= 0){
-            this.itemsSelecionados[index].quantidade++;
-          } else {
- 
-          this.itemsSelecionados.push({
-            nome: _text,
-            codigoBarras: _text,
-            quantidade: 1
-          });
-          }
+          this.adicionarProduto(_text)
         });
     };
+
+    this.speech = speechModule;
+  }
+
+  adicionarProduto(codBarras){
+    if(!codBarras) return;
+     this.itemsSelecionados  = this.itemsSelecionados ? this.itemsSelecionados : [];
+          
+    let index = this.itemsSelecionados.findIndex((item) => {
+      return item.codigoBarras == codBarras
+    })
+
+    if(index >= 0){
+      this.itemsSelecionados[index].quantidade++;
+    } else {
+
+    this.itemsSelecionados.push({
+      nome: codBarras,
+      codigoBarras: codBarras,
+      quantidade: 1
+    });
+    }
   }
 
   deletar(event: any, index : any) {
@@ -56,9 +66,69 @@ export class CarrinhoCompras {
       listaFormatada: this.formatarString()
     })
   }
+
+  mostraModalConfirmacao(melhorPalavra){
+    let confirm = this.alertCtrl.create({
+      title: 'Confirmação de coódigo de barras',
+      message: 'Confira, e altere caso necessário o codigo de Barras. O Código de barras confere?',
+      inputs: [
+      {
+        name: 'melhorPalavra',
+        placeholder: 'Cod. Barras',
+        value:melhorPalavra
+      }],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Não, Repetir Teste',
+          handler: () => {
+            this.tratarEntradaDeVoz();
+          }
+        },
+        {
+          text: 'Sim',
+          handler: (data) => {
+            this.adicionarProduto(data.melhorPalavra);
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
   
-  abrirTelaVoz(){
-    this.navCtrl.push(VozPage);
+  tratarEntradaDeVoz(){
+    this.speech.hasPermission().then(result => {
+      if(!result) this.speech.requestPermission().then(() => this.tratarEntradaDeVoz());
+      else{
+        this.speech.startListening().subscribe(data => {
+          if(data && data.length){
+            console.log(data)
+            let melhorCaso;
+            for(let i = 0; i < data.length; i++){
+              if(data[i][0] >= '0' && data[i][0] <= '9'){
+                melhorCaso = data[i];
+              }
+            }
+
+            melhorCaso = melhorCaso.trim();
+            var aux = "";
+            var i = 0
+            while (i < melhorCaso.length) {
+              if(melhorCaso[i] >= '0' && melhorCaso[i] <= '9'){
+                aux += melhorCaso[i];
+              }
+              i++;
+            };
+            
+            this.mostraModalConfirmacao(aux);
+          }
+        }, error => console.log(error));
+      }
+    })  
   }
 
   alteraQuantidade(index:number, alteracao:number) {
